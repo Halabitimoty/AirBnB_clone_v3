1b861ef6b1bdfc75a791e5dee374245e25aeccb8
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2023-05-19 09:46:53 +0100

    Airbnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 This module contains the principal application
@@ -39,6 +38,6 @@ Swagger(app)
 if __name__ == "__main__":
 
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5002)
+    port = getenv('HBNB_API_PORT', default=5000)
 
     app.run(host, int(port), threaded=True)
